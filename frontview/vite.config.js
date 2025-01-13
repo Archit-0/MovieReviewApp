f13cdb9457bbdd9da6fe1f22feb9aa23a65acda1
Archit <archit0422chatan@gmail.com>
2025-01-13 20:39:54 +0530

    vite config file

--- frontview/vite.config.js
+++ frontview/vite.config.js
@@ -6,22 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": {
-        target:
-          import.meta.env.MODE === "production"
-            ? "https://your-backend-url.vercel.app"
-            : "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-      "/uploads/": {
-        target:
-          import.meta.env.MODE === "production"
-            ? "https://your-backend-url.vercel.app"
-            : "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
+      "/api/": "http://localhost:3000",
+      "/uploads/": "http://localhost:3000",
     },
   },
 });
